810ba86adb0ab6b147272afc1613389c553bd970
Batool ALDALKI <gt.batool.aldalki@orange.com>
2025-03-12 17:02:23 +0300

    Add project files.

---
+++ angularapp13.client/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+
+  { path: "loginAli", component: SignInComponent },
+  { path: "signup", component: SignUpComponent },
+  {path : "home" , component : HomeComponent}
+
+
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angularapp13.client/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({ //  Manager
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent // coach batool
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
